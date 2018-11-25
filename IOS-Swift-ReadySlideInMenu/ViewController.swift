4ffffd3c6b01283563873a015ef26088d76fa6bb
Pooya Hatami <am.hatami@gmail.com>
2018-11-24 20:35:38 -0800

    Initial Commit

---
+++ IOS-Swift-ReadySlideInMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS-Swift-ReadySlideInMenu
+//
+//  Created by Pooya on 2018-11-24.
+//  Copyright © 2018 Sooni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
